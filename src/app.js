f50d45bc624a4feaa26424a770871cfe74789c22
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2020-03-09 18:13:36 -0700

    Templates

--- src/app.js
+++ src/app.js
@@ -1,28 +1,65 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+// Setup handlebars engine and views location 
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-    res.send([
-        {
-        name: 'Fernanda',
-        age: 32
-        }
-    ])
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Fernanda Silva'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
+    res.render('about', {
+        title: 'About Me',
+        name: 'Fernanda Silva'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'help',
+        name: 'Fernanda Silva'
+    })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
-        forecast: 'It is sunny',
-        location: 'San Francisco'
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Fernanda Silva',
+        errorMessage: 'Help article not found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Fernanda Silva',
+        errorMessage: 'Page not found!'
     })
 })
 
