cc83215edaa1fdbdd04b7e924ed0361532b8bd32
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2020-03-11 00:15:13 -0700

    Weather form

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -41,9 +43,43 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+
+    if(!req.query.address){
+        return res.send({
+            error: 'You most provide an address!'
+        })
+    }
+
+    geocode(`${req.query.address}`, (error, { latitude, longitude, location } = {}) => {
+        if(error){
+            console.log(error)
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                console.log('forecast', error)
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'You most provide a search term!'
+        })
+    }
+    
+    console.log(req.query.search)
     res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
+        products: []
     })
 })
 
